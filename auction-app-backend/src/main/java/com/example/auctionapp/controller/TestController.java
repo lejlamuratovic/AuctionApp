62e0e56fb51d4001e81190073bf8d96e94128847
lejlamrt <lejlamuratovic6@gmail.com>
2024-03-12 16:28:14 +0100

    backend structure setup

---
+++ auction-app-backend/src/main/java/com/example/auctionapp/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.auctionapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping(method = RequestMethod.GET, path = "/test")
+    public String controllerTest() {
+        return "Hello world!";
+    }
+}
