f7ea521967ff2010288712a0992ed0b4a94ff526
lejlamrt <lejlamuratovic6@gmail.com>
2024-03-18 13:11:02 +0100

    terms and conditions page

--- auction-app-frontend/src/App.jsx
+++ auction-app-frontend/src/App.jsx
@@ -2,14 +2,16 @@ import "scss/index.scss";
 import { WhiteNavbar } from "./components/WhiteNavbar";
 import { BlackNavbar } from "./components/BlackNavbar";
 import { Footer } from "./components/Footer";
-import { AboutUs } from "./pages/AboutUs";
+import { AboutUs } from "./pages";
+import { TermsAndConditions } from "./pages";
 
 const App = () => {
   return (
     <>
       {/* <BlackNavbar /> */}
       {/* <WhiteNavbar /> */}
-      <AboutUs />
+      {/* <AboutUs /> */}
+      <TermsAndConditions />
       {/* <Footer /> */}
     </>
   );
--- auction-app-frontend/src/pages/index.js
+++ auction-app-frontend/src/pages/index.js
@@ -1,1 +1,3 @@
-export { default as TestPage } from './TestPage'
+export { default as TestPage } from "./TestPage";
+export { default as TermsAndConditions } from "./TermsAndConditions/TermsAndConditions";
+export { default as AboutUs } from "./AboutUs/AboutUs";
