189047e9907f1dbc753acbe40d2f4748e569d92d
lejlamrt <lejlamuratovic6@gmail.com>
2024-03-20 19:26:32 +0100

    organizing scss

--- auction-app-frontend/src/components/Footer/Footer.jsx
+++ auction-app-frontend/src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-column body-regular">
         <div className="column-heading">
-          <p>Auction</p>
+          <span>Auction</span>
         </div>
         <div className="column-content">
           <Link to="/about-us">About Us</Link>
@@ -20,11 +20,11 @@ const Footer = () => {
 
       <div className="footer-column body-regular">
         <div className="column-heading">
-          <p>Get in touch</p>
+          <span>Get in touch</span>
         </div>
         <div className="column-content">
-          <p>Call Us at +123 797-567-2535</p>
-          <p>support@auction.com</p>
+          <span>Call Us at +123 797-567-2535</span>
+          <span>support@auction.com</span>
           <SocialMediaIcons />
         </div>
       </div>
