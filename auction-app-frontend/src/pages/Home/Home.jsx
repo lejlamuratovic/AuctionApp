44484843c7d161783219b0478c651b99be3fa0e4
lejlamrt <lejlamuratovic6@gmail.com>
2024-03-22 19:42:05 +0100

    category list style

--- auction-app-frontend/src/pages/Home/Home.jsx
+++ auction-app-frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
-import { Link } from "react-router-dom";
+import "./style.scss";
+
+import { CATEGORIES } from "src/constants";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <div className="categories body-regular">
+        <div className="categories-heading">Categories</div>
+        <ul>
+          {/* hardcoded for now */}
+          {CATEGORIES.map((category, index) => (
+            <li key={index}>{category}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Home;
