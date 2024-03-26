82f01319fb82a3be65b55d89cb6f66ce99ca8f5f
lejlamrt <lejlamuratovic6@gmail.com>
2024-03-26 20:00:56 +0100

    switched to infinte scroll hook package

--- auction-app-frontend/src/components/ProductGrid/ProductGrid.jsx
+++ auction-app-frontend/src/components/ProductGrid/ProductGrid.jsx
@@ -1,22 +1,27 @@
-import InfiniteScroll from "react-infinite-scroll-component";
+import useInfiniteScroll from "react-infinite-scroll-hook";
 
 import { ProductCard, LoadingComponent } from "src/components";
-
 import "./style.scss";
 
 const ProductGrid = ({ items, fetchMoreData, hasMore, loading }) => {
+  const [sentryRef] = useInfiniteScroll({
+    loading,
+    hasNextPage: hasMore,
+    onLoadMore: fetchMoreData,
+    rootMargin: "0px 0px 400px 0px",
+  });
+
   return (
-    <InfiniteScroll
-      dataLength={items.length}
-      next={fetchMoreData}
-      hasMore={hasMore}
-      loader={<LoadingComponent />}
-      className="product-grid"
-    >
+    <div className="product-grid">
       {items.map((item) => (
         <ProductCard key={item.id} {...item} />
       ))}
-    </InfiniteScroll>
+      {(loading || hasMore) && (
+        <div ref={sentryRef}>
+          <LoadingComponent />
+        </div>
+      )}
+    </div>
   );
 };
 
