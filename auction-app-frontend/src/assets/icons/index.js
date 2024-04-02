8fa92586149ba6d4dfbd5834bb732b73217cf60b
lejlamrt <lejlamuratovic6@gmail.com>
2024-04-02 14:44:08 +0200

    search bar added

--- auction-app-frontend/src/assets/icons/index.js
+++ auction-app-frontend/src/assets/icons/index.js
@@ -3,5 +3,6 @@ import instagram from "./instagram.svg";
 import twitter from "./twitter.svg";
 import next from "./next.svg";
 import go from "./go.svg";
+import search from "./search.svg";
 
-export { facebook, instagram, twitter, next, go };
+export { facebook, instagram, twitter, next, go, search };
--- auction-app-frontend/src/components/Navbar/Navbar.jsx
+++ auction-app-frontend/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { NavLink, Link } from "react-router-dom";
 
 import { NAV_ITEMS } from "src/constants";
 import { logo } from "src/assets/images";
+import { search } from "src/assets/icons";
 
 import "./style.scss";
 
@@ -12,6 +13,16 @@ const Navbar = () => {
         <Link to="/">
           <img src={ logo } alt="Auction App Logo" />
         </Link>
+      </div>
+      <div className="search-bar">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Try enter: Shoes"
+        />
+        <button className="search-button">
+          <img src={ search } alt="Search Button" />
+        </button>
       </div>
       <div className="navbar-items body-regular">
         { NAV_ITEMS.map((item) => (
