867dca5614911883340c58bf894dc0a6b8bbef8b
lejlamrt <lejlamuratovic6@gmail.com>
2024-05-03 21:43:51 +0200

    profile forms updated

--- auction-app-frontend/src/components/ProfileTab/ProfileTab.jsx
+++ auction-app-frontend/src/components/ProfileTab/ProfileTab.jsx
@@ -1,17 +1,25 @@
-import { useForm } from "react-hook-form"
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-import { Button, FormContainer } from "src/components"
+import { Button, FormContainer, InputField } from "src/components";
 
-import { userProfilePicture } from "src/assets/images"
-import { BUTTON_LABELS, BUTTON_VARIANTS } from "src/constants"
-import { personalInformationFormFields } from "src/forms/fields"
+import { userProfilePicture } from "src/assets/images";
+import { dropdownInactive, dropdownActive } from "src/assets/icons";
+import { BUTTON_LABELS, BUTTON_VARIANTS } from "src/constants";
+import { personalInformationFormFields, cardInformationFields, addressInformationFields } from "src/forms/fields";
 
-import "./style.scss"
+import "./style.scss";
 
 const ProfileTab = () => {
     const methods = useForm({
         mode: "onBlur"
-    }); 
+    });
+
+    const [showCardInfo, setShowCardInfo] = useState(false);
+    const [showAddressInfo, setShowAddressInfo] = useState(false);
+
+    const toggleAdditionalInfo1 = () => setShowCardInfo(!showCardInfo);
+    const toggleAdditionalInfo2 = () => setShowAddressInfo(!showAddressInfo);
 
     const onSubmit = (data) => {
         console.log(data);
@@ -19,28 +27,80 @@ const ProfileTab = () => {
 
     return (
         <div className="profile-tab-container">
-            <div className="general-information">
-                <div className="general-information-header">
-                    <span className="body-semibold">Personal Information</span>
-                </div>
-                <div className="general-information-columns">
-                    <div className="general-information-column">
-                        <img src={ userProfilePicture } alt="Profile Picture Holder" className="user-profile-picture"/>
-                        <Button label={ BUTTON_LABELS.CHANGE_PHOTO } variant={ BUTTON_VARIANTS.OUTLINED }> </Button>
+            <FormContainer 
+                onSubmit={ onSubmit } 
+                methods={ methods } 
+                buttonLabel={ BUTTON_LABELS.SAVE_INFO }
+                buttonVariant={ BUTTON_VARIANTS.OUTLINED }
+            >
+                {/* general information */}
+                <div className="general-information">
+                    <div className="general-information-header">
+                        <span className="body-semibold">Personal Information</span>
                     </div>
-                    <div className="general-information-column">
-                        <FormContainer 
-                            formFields={ personalInformationFormFields } 
-                            onSubmit={ methods.handleSubmit(onSubmit) } 
-                            buttonLabel={ BUTTON_LABELS.SAVE_INFO }
-                            buttonVariant={ BUTTON_VARIANTS.OUTLINED }
-                            methods={ methods }
-                        />
+                    <div className="general-information-columns">
+                        <div className="general-information-column">
+                            <img src={ userProfilePicture } alt="Profile Picture" className="user-profile-picture"/>
+                            <Button label={ BUTTON_LABELS.CHANGE_PHOTO } variant={ BUTTON_VARIANTS.OUTLINED } />
+                        </div>
+                        <div className="general-information-column">
+                            { personalInformationFormFields.map(field => (
+                                <InputField
+                                    key={ field.name }
+                                    name={ field.name }
+                                    label={ field.label }
+                                    type={ field.type }
+                                    rules={ field.rules }
+                                    options={ field.type === "select" ? field.options : null }
+                                />
+                            )) }
+                        </div>
                     </div>
                 </div>
-            </div>
+                {/* collapsible sections */}
+                <div className="general-information">
+                    <div className="general-information-header" onClick={ toggleAdditionalInfo1 }>
+                        <img src={showCardInfo ? dropdownActive : dropdownInactive} alt="Toggle Additional Information" />
+                        <span className="body-semibold">Card Information (Optional)</span>
+                    </div>
+                    { showCardInfo && cardInformationFields.map(field => (
+                        <div className="collapsable-column">
+                            <div className="collapsable-form">
+                                <InputField
+                                    key={ field.name }
+                                    name={ field.name }
+                                    label={ field.label }
+                                    type={ field.type }
+                                    rules={ field.rules }
+                                    options={ field.type === "select" ? field.options : null }
+                                />
+                            </div>
+                        </div>
+                    )) }
+                </div>
+                <div className="general-information">
+                    <div className="general-information-header" onClick={ toggleAdditionalInfo2 }>
+                        <img src={ showAddressInfo ? dropdownActive : dropdownInactive } alt="Toggle Additional Information" />
+                        <span className="body-semibold">Shipping Address (Optional)</span>
+                    </div>
+                    { showAddressInfo && addressInformationFields.map(field => (
+                        <div className="collapsable-column">
+                            <div className="collapsable-form">
+                                <InputField
+                                    key={ field.name }
+                                    name={ field.name }
+                                    label={ field.label }
+                                    type={ field.type }
+                                    rules={ field.rules }
+                                    options={ field.type === "select" ? field.options : null }
+                                />
+                            </div>
+                        </div>
+                    )) }
+                </div>
+            </FormContainer>
         </div>
-    )
+    );
 }
 
 export default ProfileTab;
--- auction-app-frontend/src/forms/fields/index.js
+++ auction-app-frontend/src/forms/fields/index.js
@@ -4,4 +4,9 @@ export * from "./registerFormFields.js"
 export * from "./placeBidFormFields.js"
 =======
 export * from "./personalInformationFormFields.js"
+<<<<<<< HEAD
 >>>>>>> 9013752 (personal information form)
+=======
+export * from "./addressInformationFields.js"
+export * from "./cardInformationFields.js"
+>>>>>>> 6cb6395 (profile forms updated)
