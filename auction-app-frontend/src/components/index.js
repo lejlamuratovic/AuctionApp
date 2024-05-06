634562cd1529c49fae6c1f8ba3d4f9200ad3f35e
lejlamrt <lejlamuratovic6@gmail.com>
2024-05-06 11:40:06 +0200

    import fix

--- auction-app-frontend/src/components/index.js
+++ auction-app-frontend/src/components/index.js
@@ -18,8 +18,6 @@ export { default as LoginForm } from "./LoginForm/LoginForm";
 export { default as RegisterForm } from "./RegisterForm/RegisterForm";
 export { default as Notifications } from "./Notifications/Notifications";
 export { default as MyAccountOptions } from "./MyAccountOptions/MyAccountOptions";
-export { default as PersonalInformation } from "./PersonalInformation/PersonalInformation";
-export { default as MyAccountOptions } from "./MyAccountOptions/MyAccountOptions";
 export { default as ProfileTab } from "./ProfileTab/ProfileTab";
 export { default as SellerTab } from "./SellerTab/SellerTab";
 export { default as ProductBidsTable } from "./ProductBidsTable/ProductBidsTable";
