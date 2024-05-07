ea4cf22202414230d87086d3f48844bedccf624a
lejlamrt <lejlamuratovic6@gmail.com>
2024-05-07 12:07:24 +0200

    customized select field

--- auction-app-backend/src/main/java/com/example/auctionapp/repository/BidRepository.java
+++ auction-app-backend/src/main/java/com/example/auctionapp/repository/BidRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public interface BidRepository extends JpaRepository<BidEntity, UUID> {
     BidEntity findTopBidEntityByProductEntity_ProductIdOrderByBidTimeDesc(final UUID productId);
-    List<BidEntity> findBidEntitiesByUserEntity_UserId(final UUID userId);
-    @Query("SELECT b FROM BidEntity b " +
-            "WHERE b.userEntity.userId = :userId AND b.bidAmount = (SELECT MAX(b2.bidAmount) " +
-            "FROM BidEntity b2 WHERE b2.productEntity.productId = b.productEntity.productId)")
+    @Query("SELECT b FROM BidEntity b WHERE b.userEntity.userId = :userId " +
+            "AND b.bidAmount = (SELECT MAX(b2.bidAmount) FROM BidEntity b2 " +
+            "WHERE b2.productEntity.productId = b.productEntity.productId AND b2.userEntity.userId = :userId) " +
+            "GROUP BY b.productEntity.productId, b.bidId")
     List<BidEntity> findDistinctHighestBidsByUserId(UUID userId);
 }
--- auction-app-frontend/src/components/SelectField/SelectField.jsx
+++ auction-app-frontend/src/components/SelectField/SelectField.jsx
@@ -1,28 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useFormContext } from "react-hook-form";
+
+import { dropdown } from "src/assets/icons";
 
 import "./style.scss";
 
-const SelectField = ({ name, options, rules }) => {
+const SelectField = ({ name, options, rules, label }) => {
     const { register, formState: { errors } } = useFormContext();
     
-    const [selected, setSelected] = useState(options[0].value);
+    const [selected, setSelected] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
 
-    const handleChange = (event) => {
-        setSelected(event.target.value);
-        register(name, { value: event.target.value, ...rules });
+    const toggleDropdown =  () => setIsOpen(!isOpen);
+
+    const handleSelect = (value) => {
+        setSelected(value);
+        setValue(name, value);
+        setIsOpen(false);
     };
 
+    useEffect(() => {
+        // register field
+        register(name, { ...rules });
+    }, [register, name, rules]);
+    
+
+    const displayLabel = selected ? options.find(option => option.value === selected)?.label : label;
+
     return (
-        <div className="custom-select">
-            <select name={ name } value={ selected } onChange={ handleChange } className={ errors[name] ? "error" : "" }>
-                { options.map(option => (
-                    <option key={ option.value } value={ option.value }>
-                        { option.label }
-                    </option>
-                )) }
-            </select>
-            { errors[name] && <span className="error-message body-small-regular">{ errors[name].message }</span> }
+        <div className="custom-select body-regular">
+            <div className="dropdown-btn" onClick={ toggleDropdown }>
+                { displayLabel }
+                <img src={ dropdown } alt="Dropdown" />
+            </div>
+            { isOpen && (
+                <div className="dropdown-content">
+                    { options.map(option => (
+                        <div key={ option.value } className="dropdown-item" onClick={ () => handleSelect(option.value) }>
+                            { option.label }
+                        </div>
+                    )) }
+                </div>
+            ) }
+            { errors[name] && <span className="error-message">{ errors[name].message }</span> }
         </div>
     );
 };
