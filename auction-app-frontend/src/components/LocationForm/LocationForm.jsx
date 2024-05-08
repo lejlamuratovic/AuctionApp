2aead76d6243404c891b4dd125f21938b8315fb4
lejlamrt <lejlamuratovic6@gmail.com>
2024-05-08 20:01:22 +0200

    card fields on shipping form

--- auction-app-frontend/src/components/LocationForm/LocationForm.jsx
+++ auction-app-frontend/src/components/LocationForm/LocationForm.jsx
@@ -1,53 +1,70 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 import { FormContainer } from "src/components";
 
-import { locationShippingFormFields } from "src/forms/fields";
+import { locationShippingFormFields, cardInformationFields } from "src/forms/fields";
 import { BUTTON_LABELS, ROUTE_PATHS } from "src/constants";
 
-import "./style.scss"
+import "./style.scss";
 
 const LocationForm = ({ formData, setFormData, handleFinalSubmit }) => {
     const navigate = useNavigate();
+
+    const [showCardInfo, setShowCardInfo] = useState(false);
 
     const methods = useForm({
         mode: "onBlur", 
         defaultValues: formData
     });
 
-    const onSubmit = (data) => {
-        setFormData(data);
-        handleFinalSubmit();
+    const onSubmit = () => {
+        const data = methods.getValues();
+
+        if (!showCardInfo) {
+            setFormData({...formData, ...data});
+            setShowCardInfo(true);
+        } else {
+            setFormData({...formData, ...data});
+            handleFinalSubmit();
+        }
     };
 
     const onCancel = () => {
         navigate(ROUTE_PATHS.MY_ACCOUNT);
-    }
+    };
 
     const onBack = () => {
-        navigate("#prices");
-    }
+        if (showCardInfo) {
+            setShowCardInfo(false);
+        } else {
+            navigate("#prices");
+        }
+    };
+
+    const combinedFormFields = showCardInfo ? 
+        [...locationShippingFormFields, ...cardInformationFields] : locationShippingFormFields;
 
     return (
         <div className="location-form form">
             <div className="form-header">
-                <h5> LOCATION & SHIPPING </h5>
+                <h5>LOCATION & SHIPPING</h5>
             </div>
             <div className="form-fields">
                 <FormContainer 
-                    formFields={ locationShippingFormFields } 
-                    onSubmit={ methods.handleSubmit(onSubmit) } 
+                    formFields={ combinedFormFields }
+                    onSubmit={ methods.handleSubmit(onSubmit) }
                     onCancel={ onCancel }
-                    onBack = { onBack }
+                    onBack={ onBack }
                     buttonLabel={ BUTTON_LABELS.NEXT }
                     cancelLabel={ BUTTON_LABELS.CANCEL }
                     backLabel={ BUTTON_LABELS.BACK }
-                    methods={ methods }
+                    methods= {methods }
                 />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default LocationForm;
