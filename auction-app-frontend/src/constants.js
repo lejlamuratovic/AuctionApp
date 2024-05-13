0a1bc4fd6ec410c27ff7defaec18b551973fa7c3
lejlamrt <lejlamuratovic6@gmail.com>
2024-05-13 19:18:59 +0200

    frontend env variables

--- auction-app-frontend/src/constants.js
+++ auction-app-frontend/src/constants.js
@@ -11,9 +11,9 @@ import {
   cart
 } from "src/assets/icons";
 
-export const BASE_URL = "https://auctionapp-6r6v.onrender.com/api/v1";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const WEB_SOCKET_BASE_URL = "wss://auctionapp-6r6v.onrender.com/websocket";
+export const WEB_SOCKET_BASE_URL = import.meta.env.VITE_WEB_SOCKET_BASE_URL;
 
 export const NAV_ITEMS = [
   {
