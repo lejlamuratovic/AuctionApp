389a5209cd69a50c8ecbe252c0796cd8655fb2d1
lejlamrt <lejlamuratovic6@gmail.com>
2024-05-23 13:06:24 +0200

    sending requests to stripe

--- auction-app-frontend/src/components/CheckoutComponent/CheckoutComponent.jsx
+++ auction-app-frontend/src/components/CheckoutComponent/CheckoutComponent.jsx
@@ -1,15 +1,50 @@
-import { Elements } from "@stripe/react-stripe-js"; 
-import { loadStripe } from "@stripe/stripe-js"; 
+import { useState, useEffect } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-import { CheckoutForm } from "src/components";
+import { CheckoutForm, LoadingComponent, ErrorComponent } from "src/components";
 import { STRIPE_PUBLIC_KEY } from "src/constants";
+import { createPaymentIntent } from "src/services";
 
 const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
 
 const CheckoutComponent = () => {
+    const [clientSecret, setClientSecret] = useState();
+    const [loading, setLoading] = useState();
+    const [errorMessage, setErrorMessage] = useState();
+
+    const fetchPaymentIntent = () => {
+        // dummy charge request
+        const chargeRequest = {
+            customerEmail: "johndoe123@example.com",
+            customerName: "John Doe",
+            product: { highestBid: 2000 }
+        };
+
+        setLoading(true);
+
+        createPaymentIntent(chargeRequest)
+            .then((response) => {
+                if (response.clientSecret) {
+                    setClientSecret(response.clientSecret);
+                    setLoading(false);
+                }
+            }).catch((error) => {
+                setErrorMessage(error.message);
+                setLoading(false);
+            });
+    }
+                    
+    useEffect(() => {
+        fetchPaymentIntent();
+    }, []);
+
+    if (loading) return <LoadingComponent />
+    if (errorMessage) return <ErrorComponent message={ errorMessage } />
+
     return (
-        <Elements stripe={ stripePromise }>
-            <CheckoutForm />
+        <Elements stripe={ stripePromise }  options={{ clientSecret }}>
+            <CheckoutForm clientSecret={ clientSecret } />
         </Elements>
     );
 };
--- auction-app-frontend/src/components/CheckoutForm/CheckoutForm.jsx
+++ auction-app-frontend/src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,94 +1,61 @@
-import { useState, useEffect } from "react";
-import {
-  Elements,
-  PaymentElement,
-  useStripe,
-  useElements
-} from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+import { useState } from "react";
+import { PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
-import { LoadingComponent, Button } from "src/components";
+import { LoadingComponent, Button, ErrorComponent } from "src/components";
+import { BUTTON_VARIANTS, BUTTON_LABELS } from "src/constants";
 
-import { createPaymentIntent } from "src/services";
-import { STRIPE_PUBLIC_KEY, BUTTON_VARIANTS, BUTTON_LABELS } from "src/constants";
+import "./style.scss";
 
-import "./style.scss";  
-
-const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
-
-const CheckoutForm = () => {
-    const [clientSecret, setClientSecret] = useState();
+const CheckoutForm = ({ clientSecret }) => {
     const [errorMessage, setErrorMessage] = useState();
-    const [loading, setLoading] = useState(true);
+    const [successMessage, setSuccessMessage] = useState();
 
-    useEffect(() => {
-        const chargeRequest = {
-            customerEmail: "johndoe@example.com",
-            customerName: "John Doe",
-            product: {
-                highestBid: 1000
-            }
-        };
-
-        fetchClientSecret(chargeRequest);
-    }, []);
-
-    const fetchClientSecret = (chargeRequest) => {
-        createPaymentIntent(chargeRequest)
-            .then((response) => {
-                if (response.clientSecret) {  
-                    setClientSecret(response.clientSecret);
-                    setLoading(false); 
-                } else {
-                    setErrorMessage("Failed to fetch client secret");
-                }
-            }).catch((error) => {
-                setErrorMessage(error.message);
-                setLoading(false);
-            });
-    };
-
+    // initialize stripe and elements
     const stripe = useStripe();
     const elements = useElements();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-
+    
         if (!stripe || !elements) {
+            setErrorMessage("Stripe has not initialized");
             return;
         }
-
-        const confirmResult = await stripe.confirmPayment({
-            elements,
-            clientSecret,
-            confirmParams: {
-                // sample return URL
-                // return_url: "https://example.com/order/123/complete",
-            },
-        });
-
-        if (confirmResult.error) {
-            setErrorMessage(confirmResult.error.message);
-        } else {
-            console.log("Payment succeeded!");
+    
+        try {
+            // confirm payment with stripe elements and client secret from parent component
+            const confirmResult = await stripe.confirmPayment({
+                elements,
+                confirmParams: {
+                    // return url
+                },
+                redirect: "if_required",
+            });
+    
+            if (confirmResult.error) {
+                setErrorMessage(confirmResult.error.message);
+            } else {
+                setSuccessMessage("Payment confirmed successfully");
+            }
+        } catch (error) {
+            setErrorMessage(error.message || "An error occurred during payment confirmation.");
         }
     };
-
-    if (loading) return <LoadingComponent />;
+    
+    if (!stripe || !elements) return <LoadingComponent />;
+    if (successMessage) return <div>{ successMessage }</div>;
     if (errorMessage) return <ErrorComponent message={ errorMessage } />;
-
+    
     return (
         <div className="form-container">
             <form onSubmit={ handleSubmit }>
-                <Elements stripe={ stripePromise } options={{ clientSecret }}>
-                    <PaymentElement />
-                    <Button 
-                        variant={ BUTTON_VARIANTS.FILLED }
-                        label={ BUTTON_LABELS.PAY }
-                        disabled={ !stripe || !elements }
-                        type="submit"
-                    />
-                </Elements>
+                <PaymentElement />
+                <Button
+                    variant={ BUTTON_VARIANTS.FILLED }
+                    label={ BUTTON_LABELS.PAY }
+                    disabled={ !stripe || !elements }
+                    type="submit"
+                />
             </form>
         </div>
     );
