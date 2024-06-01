5f223a3aa3db6250527ebf96df90f771d4cf8116
lejlamrt <lejlamuratovic6@gmail.com>
2024-06-01 17:21:29 +0200

    my account

--- auction-app-frontend/src/components/SettingsTab/SettingsTab.jsx
+++ auction-app-frontend/src/components/SettingsTab/SettingsTab.jsx
@@ -1,36 +1,58 @@
-import { Button, Checkbox } from "src/components";
+import { useEffect, useState } from "react";
 
-import { BUTTON_LABELS, POLICY_AND_COMMUNITY } from "src/constants";
+import { Button } from "src/components";
+
+import { BUTTON_LABELS } from "src/constants";
+import { useUser } from "src/store/UserContext";
+import { getUser } from "src/services/userService";
 
 import "./style.scss"
 
 const SettingsTab = () => {
-  return (
-    <div className="settings-tab-container">
-        <div className="box-container body-semibold">
-            <div className="box top-left">
-                <span className="box-heading">Policy and Community</span>
-                <span className="box-description">Receive updates on bids and seller's offers. Stay informed through:</span>
-                <div className="checkbox-container body-regular">
-                    { POLICY_AND_COMMUNITY.map((item) => (
-                        <Checkbox key={ item.id } label={ item.label } />
-                    )) }
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const { userId } = useUser();
+  
+    const fetchUserInformation = () => {
+      setLoading(true);
+  
+      getUser(userId)
+        .then((response) => {
+          setUser(response);
+          setLoading(false);
+          console.log("User information fetched successfully", response);
+        })
+        .catch((error) => {
+          setError(error);
+          setLoading(false);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  
+    useEffect(() => {
+      if (userId) fetchUserInformation();
+    }, [userId]);
+
+    return (
+        <div className="settings-tab-container">
+            <div className="box-container body-semibold">
+                <div className="box top-right">
+                    <span className="box-heading">Contact Information</span>
+                    <span className="box-description">This information can be edited on your profile.</span>
+                    <span className="box-heading">Email <span className="box-information body-semibold">{ user?.email }</span></span>
+                </div>
+                <div className="box bottom-left">
+                    <span className="box-heading">Account Information</span>
+                    <span className="box-description">Do you want to deactivate account?</span>
+                    <Button label={ BUTTON_LABELS.DEACTIVATE } disabled={ true }/>
                 </div>
             </div>
-            <div className="box top-right">
-                <span className="box-heading">Contact Information</span>
-                <span className="box-description">This information can be edited on your profile.</span>
-                <span className="box-heading">Email <span className="box-information body-semibold">johndoe@gmail.com</span></span>
-                <span className="box-heading">Phone <span className="box-information body-semibold">555 555 555</span></span>
-            </div>
-            <div className="box bottom-left">
-                <span className="box-heading">Account Information</span>
-                <span className="box-description">Do you want to deactivate account?</span>
-                <Button label={ BUTTON_LABELS.DEACTIVATE } disabled={ true }/>
-            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default SettingsTab;
